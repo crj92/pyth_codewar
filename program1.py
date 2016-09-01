3b9108ded6388a5ceecce8ec11509c0a8a68a9fd
Raj Shekhar <raj.1992.shekhar@gmail.com>
2016-09-01 15:26:24 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,42 @@
+def likes(names):
+    # your code here
+    if len(names) == 0:
+        print "'no one likes this'"
+    elif len(names) == 1:
+        print '\''+', '.join(names), "likes this'"
+    elif len(names) <= 3:
+        print '\''+', '.join(names), "like this'"
+    else:
+        print '\''+', '.join(names[0:2]), "and", int(len(names) - 2), "other like this'" 
+
+        print names[0:2]
+def main():
+    print "inside main"
+    
+    list1 = []
+    
+    print "enter names or # to exit"
+    for i in range(0, 999):
+        list1.append(raw_input("Enter name: "));
+        if list1[i] == '#':break
+    list1.remove('#')
+    likes(list1)
+if __name__ == "__main__":
+    main()
+
+
+
+"""if __name__=="__likes__":
+    likes(str(sys.argv))"""
+  
+  
+  
+"""#your code here
+print "in method"
+list1 = [1,2,3,4];
+length=len(list1)
+print length
+list1.pop(0)
+print list1
+for i in list1:
+    print i"""
